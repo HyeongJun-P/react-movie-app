1aee7f63ccf52471289fa5bc8c849fff1013f2f5
765prod <80656271+765prod@users.noreply.github.com>
2021-04-15 19:14:29 +0900

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import React from 'react';
 import Potato from './components/Potato';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <Potato />
-    </div>
-  );
+  
 }
 
 export default App;
